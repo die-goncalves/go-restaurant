5445facbe3620ab7583f994f0a28e83ab2b02b69
die-goncalves <die.goncalves1990@gmail.com>
2022-12-16 16:16:29 -0300

    feat: add barlow semi condensed font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Barlow_Semi_Condensed } from '@next/font/google'
+
+const barlow_semi_condensed = Barlow_Semi_Condensed({
+  weight: ['300', '400', '500', '600', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --font-barlow-semi-condensed: ${barlow_semi_condensed.style
+            .fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
