bace8eaa764d3b93a3e4439fc2e89ff58a748537
die-goncalves <die.goncalves1990@gmail.com>
2022-12-31 17:18:30 -0300

    feat: add DialogMap component

---
+++ src/components/DialogMap/MapInsideDialog.tsx
@@ -0,0 +1,155 @@
+import clsx from 'clsx'
+import { useEffect, useRef } from 'react'
+import { MapPin } from 'phosphor-react'
+import mapboxgl from 'mapbox-gl'
+import { useFilter } from '../../contexts/FilterContext'
+import { decodeGeohash } from '../../utils/geohash'
+import { municipalitiesCovered } from '../../../municipalitiesCovered'
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+type MapInsideDialogProps = {
+  onCoordinatesChange: (coord: { lat: number; lng: number }) => void
+}
+export default function MapInsideDialog({
+  onCoordinatesChange
+}: MapInsideDialogProps) {
+  const { state } = useFilter()
+  const mapContainer = useRef<any>(null)
+  const map = useRef<mapboxgl.Map | null>(null)
+
+  useEffect(() => {
+    map.current = new mapboxgl.Map({
+      accessToken: process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN ?? '',
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-40.25616187455981, -19.022474801531843],
+      zoom: 9
+    })
+
+    if (state.currentPosition) {
+      const { latitude, longitude } = decodeGeohash(
+        state.currentPosition.geohash
+      )
+      map.current.setCenter([longitude, latitude])
+      map.current.setZoom(15)
+    }
+
+    map.current.on('move', e => {
+      if (!map.current) return
+
+      const pinElement = document.getElementById('pin')
+
+      if (pinElement) {
+        pinElement.classList.remove('placed')
+      }
+    })
+    map.current.on('moveend', e => {
+      if (!map.current) return
+
+      const pinElement = document.getElementById('pin')
+      if (pinElement && pinElement.classList.contains('inside')) {
+        pinElement.classList.add('placed')
+        onCoordinatesChange(map.current.getCenter())
+      }
+    })
+
+    map.current.on('load', e => {
+      if (!map.current) return
+
+      map.current.addSource('municipalities-covered', {
+        type: 'geojson',
+        data: municipalitiesCovered
+      })
+
+      map.current.addLayer({
+        id: 'municipalities-fill-id',
+        type: 'fill',
+        source: 'municipalities-covered',
+        layout: {},
+        paint: {
+          'fill-color': '#f59e0b',
+          'fill-opacity': 0.1
+        }
+      })
+      map.current.addLayer({
+        id: 'municipalities-borders-id',
+        type: 'line',
+        source: 'municipalities-covered',
+        layout: {},
+        paint: {
+          'line-color': '#000000',
+          'line-width': 1
+        }
+      })
+
+      map.current.on('move', e => {
+        if (!map.current) return
+
+        const mapElement = document.getElementById('map-container')
+        let point
+        if (mapElement)
+          point = new mapboxgl.Point(
+            mapElement.offsetWidth / 2,
+            mapElement.offsetHeight / 2
+          )
+
+        const features = map.current.queryRenderedFeatures(point, {
+          layers: ['municipalities-fill-id']
+        })
+
+        if (features.length) {
+          const pinElement = document.getElementById('pin')
+          if (pinElement) pinElement.classList.add('inside')
+        } else {
+          const pinElement = document.getElementById('pin')
+          if (pinElement) pinElement.classList.remove('inside')
+        }
+      })
+    })
+
+    map.current.addControl(new mapboxgl.NavigationControl())
+    map.current.addControl(new mapboxgl.ScaleControl({ unit: 'metric' }))
+
+    map.current.resize()
+
+    return () => map.current?.remove()
+  }, [])
+
+  return (
+    <div className="relative mx-4 overflow-hidden rounded h-full">
+      <div
+        className={clsx(
+          'flex absolute overflow-visible top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 w-10 h-10'
+        )}
+      >
+        <MapPin
+          id="pin"
+          // Tamanho do ícone: 32.5px
+          // Ícone está centralizado em div de 40 de altura: 40 - 32.5 = 7.5 -> 7.5/2 = 3.25px
+          // Quero que a ponta do pin esteja no centro do div anterior: 40/2 = 20 -> 20 - 3.25 = 16.75
+          className={clsx(
+            'peer z-10 -mt-[16.75px] w-10 h-10 fill-light-orange-200 text-light-gray-800/80 [&_path]:opacity-100 [&_path]:stroke-[8px] [&_circle]:stroke-[8px]',
+            'transition-all duration-150 ease-in',
+            '[&:not(.inside)]:grayscale',
+            '[&:not(.placed)]:-translate-y-1/4',
+            '[&.placed]:-translate-y-0',
+            'drop-shadow-[2px_0_0_#292524cc]'
+          )}
+          weight="duotone"
+        />
+
+        <div
+          className={clsx(
+            "after:content-[''] after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2",
+            'after:transition after:duration-150 after:ease-in',
+            'peer-[:not(.placed)]:after:shadow-[0_0_10px_5px_rgba(0,0,0,0.5)]',
+            'peer-[.placed]:after:shadow-[0_0_5px_5px_rgba(0,0,0,0.5)]'
+          )}
+        />
+      </div>
+
+      <div id="map-container" ref={mapContainer} className="h-full" />
+    </div>
+  )
+}
---
+++ src/components/DialogMap/index.tsx
@@ -0,0 +1,137 @@
+import clsx from 'clsx'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import toast from 'react-hot-toast'
+import { GlobeHemisphereWest, X } from 'phosphor-react'
+import { useFilter } from '../../contexts/FilterContext'
+import { geographicInformation } from '../../utils/geographicInformation'
+import { encodeGeohash } from '../../utils/geohash'
+import { Dialog } from '../Dialog'
+import { TextInput } from '../TextInput'
+import MapInsideDialog from './MapInsideDialog'
+
+export function DialogMap() {
+  const router = useRouter()
+  const [open, setOpen] = useState(false)
+  const [coordinates, setCoordinates] = useState<{
+    lat: number
+    lng: number
+  }>()
+  const { state, handleTempPosition } = useFilter()
+
+  function handleSubmit() {
+    if (state.temporaryPosition) {
+      if (state.temporaryPosition.place && state.temporaryPosition.geohash) {
+        router.push(
+          `/restaurants?place=${state.temporaryPosition.place}&geohash=${state.temporaryPosition.geohash}`
+        )
+        setOpen(false)
+      } else {
+        toast.error(
+          'Não conseguimos obter as coordenadas do endereço, por favor indique no mapa.'
+        )
+      }
+    } else {
+      toast.error(
+        'Não conseguimos obter as coordenadas do endereço, por favor indique no mapa.'
+      )
+    }
+  }
+
+  function handleCoordinatesChange(coord: { lat: number; lng: number }) {
+    setCoordinates(coord)
+  }
+
+  useEffect(() => {
+    async function newGeographicCoordinates() {
+      if (coordinates) {
+        fetch(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${coordinates.lng},${coordinates.lat}.json?limit=1&access_token=${process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN}`
+        )
+          .then(response => response.json())
+          .then(data => {
+            if (data.features.length !== 0) {
+              const { granular, place, place_name } = geographicInformation(
+                data.features[0]
+              )
+              handleTempPosition({
+                coordinates: {
+                  latitude: data.features[0].geometry.coordinates[1],
+                  longitude: data.features[0].geometry.coordinates[0]
+                },
+                geohash: encodeGeohash({
+                  latitude: data.features[0].geometry.coordinates[1],
+                  longitude: data.features[0].geometry.coordinates[0]
+                }),
+                granular,
+                place_name,
+                place
+              })
+            }
+          })
+      }
+    }
+    newGeographicCoordinates()
+  }, [coordinates, handleTempPosition])
+
+  return (
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Trigger>
+        <button
+          className={clsx(
+            'flex items-center justify-between w-full px-4 py-2 bg-light-gray-200 [&:not(:disabled):hover]:bg-light-gray-300',
+            'transition-[background-color] ease-in duration-150',
+            'focus:outline-none focus:ring-2 focus:ring-inset focus:ring-light-indigo-300'
+          )}
+        >
+          <div className="flex items-center gap-2">
+            <GlobeHemisphereWest className="w-8 h-8 text-light-orange-500" />
+            <div className="flex flex-col items-start">
+              <span>Agora em</span>
+              <span>{state.currentPosition?.place}</span>
+            </div>
+          </div>
+          <span className="font-medium">Mudar</span>
+        </button>
+      </Dialog.Trigger>
+      <Dialog.Content className="flex flex-col h-3/4 w-1/3">
+        <header className="flex p-4 items-center justify-between">
+          <p className="text-xl font-medium">
+            Escolha seu novo endereço de entrega
+          </p>
+          <Dialog.Close>
+            <button
+              className={clsx(
+                'p-2 rounded bg-light-gray-200 [&:not(:disabled):hover]:bg-light-gray-300',
+                'transition-[background-color] ease-in duration-150',
+                'focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-light-indigo-300'
+              )}
+            >
+              <X className="w-6 h-6" />
+            </button>
+          </Dialog.Close>
+        </header>
+
+        <MapInsideDialog onCoordinatesChange={handleCoordinatesChange} />
+        <footer className="flex justify-center p-4 gap-4">
+          <TextInput
+            placeholder="Clique no mapa e verá o endereço aqui"
+            readOnly
+            value={state.temporaryPosition?.place_name ?? ''}
+          />
+
+          <button
+            className={clsx(
+              'ml-auto py-2 px-4 rounded bg-light-green-200 [&:not(:disabled):hover]:bg-light-green-300',
+              'transition-[background-color] ease-in duration-150',
+              'focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-light-indigo-300'
+            )}
+            onClick={handleSubmit}
+          >
+            Confirmar
+          </button>
+        </footer>
+      </Dialog.Content>
+    </Dialog.Root>
+  )
+}
