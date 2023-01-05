379e813dcb9460ecb9f0b09dae2cc83bc3130e12
die-goncalves <die.goncalves1990@gmail.com>
2023-01-05 15:32:09 -0300

    feat: add Breadcrumb component

---
+++ src/components/Breadcrumb.tsx
@@ -0,0 +1,53 @@
+import { forwardRef, ReactNode } from 'react'
+import NextLink, { LinkProps } from 'next/link'
+import { Item, List, Root } from '@radix-ui/react-navigation-menu'
+import clsx from 'clsx'
+
+type BreadcrumbComponentRootProps = {
+  children: ReactNode
+}
+const BreadcrumbComponentRoot = ({
+  children
+}: BreadcrumbComponentRootProps) => {
+  return (
+    <Root>
+      <List className="flex items-baseline">{children}</List>
+    </Root>
+  )
+}
+
+type BreadcrumbComponentLinkProps = {
+  isHomePage?: boolean
+  isCurrentPage?: boolean
+  children: ReactNode
+} & LinkProps
+const BreadcrumbComponentLink = forwardRef<
+  HTMLAnchorElement,
+  BreadcrumbComponentLinkProps
+>(({ isCurrentPage = false, isHomePage = false, ...props }, forwardedRef) => {
+  return (
+    <Item>
+      {!isHomePage ? <span>&nbsp;/&nbsp;</span> : null}
+      {isCurrentPage ? (
+        <span ref={forwardedRef} {...props} className="text-lg" />
+      ) : (
+        <NextLink
+          ref={forwardedRef}
+          {...props}
+          className={clsx(
+            'font-medium rounded hover:opacity-80 hover:underline',
+            'transition-[opacity, outline] ease-in duration-150',
+            'outline-none focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-light-indigo-300'
+          )}
+        />
+      )}
+    </Item>
+  )
+})
+
+BreadcrumbComponentLink.displayName = 'BreadcrumbLink'
+
+export const Breadcrumb = {
+  Root: BreadcrumbComponentRoot,
+  Link: BreadcrumbComponentLink
+}
