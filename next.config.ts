fc3b728f69a5eb0bd396fee28da3e24966ea3ece
die-goncalves <die.goncalves1990@gmail.com>
2026-03-02 21:37:57 -0300

    build: upgrade next, react, radix and tooling (eslint/prettier)

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
@@ -13,6 +13,6 @@ const nextConfig = {
       }
     ]
   }
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
