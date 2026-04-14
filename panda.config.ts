86b8d3367959d9b66ac24b4b74025cc947b007d8
die-goncalves <die.goncalves1990@gmail.com>
2026-04-14 14:48:17 -0300

    feat(theme): add animation styles

--- panda.config.ts
+++ panda.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from '@pandacss/dev'
+import { animationStyles } from './src/theme/animation-styles'
 import { breakpoints } from './src/theme/breakpoints'
 import { globalCss } from './src/theme/global-styles'
 import { keyframes } from './src/theme/keyframes'
@@ -21,6 +22,7 @@ export default defineConfig({
   // Useful for theme customization
   theme: {
     extend: {
+      animationStyles,
       breakpoints,
       keyframes,
       semanticTokens: {
@@ -34,15 +36,6 @@ export default defineConfig({
             value:
               'linear-gradient(90deg, #00000000 0%, #00000033 20%, #00000099 60%, #00000000 0%)'
           }
-        },
-        animations: {
-          overlayShow: { value: 'overlayShow 150ms ease-in' },
-          contentShow: { value: 'contentShow 150ms ease-in' },
-          enter: { value: 'enter 250ms ease-out' },
-          leave: { value: 'leave 250ms ease-in forwards' },
-          shimmer: { value: 'shimmer 1.2s infinite linear' },
-          fade: { value: 'fade 150ms ease-in' },
-          heartbeat: { value: 'heartbeat 2s ease-in alternate' }
         },
         zIndex: tokens.zIndexes
       }
---
+++ src/theme/animation-styles.ts
@@ -0,0 +1,144 @@
+import { defineAnimationStyles } from '@pandacss/dev'
+
+export const animationStyles = defineAnimationStyles({
+  'fade-in': {
+    value: {
+      animationName: 'fade-in',
+      animationDuration: '200ms',
+      animationTimingFunction: 'token(easings.expressive-default-effects)'
+    }
+  },
+  'fade-out': {
+    value: {
+      animationName: 'fade-out',
+      animationDuration: '150ms',
+      animationTimingFunction: 'token(easings.expressive-fast-effects)'
+    }
+  },
+  'scale-in': {
+    value: {
+      animationName: 'scale-in',
+      animationDuration: '500ms',
+      animationTimingFunction: 'token(easings.expressive-default-spatial)'
+    }
+  },
+  'scale-out': {
+    value: {
+      animationName: 'scale-out',
+      animationDuration: '350ms',
+      animationTimingFunction: 'token(easings.expressive-fast-spatial)'
+    }
+  },
+  'scale-fade-in': {
+    value: {
+      animationName: 'scale-in, fade-in',
+      animationDuration: '500ms, 200ms',
+      animationTimingFunction:
+        'token(easings.expressive-default-spatial), token(easings.expressive-default-effects)'
+    }
+  },
+  'scale-fade-out': {
+    value: {
+      animationName: 'scale-out, fade-out',
+      animationDuration: '350ms, 150ms',
+      animationTimingFunction:
+        'token(easings.expressive-fast-spatial), token(easings.expressive-fast-effects)'
+    }
+  },
+  'slide-from-top-fade-in': {
+    value: {
+      animationName: 'slide-from-top, fade-in',
+      animationDuration: '500ms, 200ms',
+      animationTimingFunction:
+        'token(easings.expressive-default-spatial), token(easings.expressive-default-effects)'
+    }
+  },
+  'slide-to-top-fade-out': {
+    value: {
+      animationName: 'slide-to-top, fade-out',
+      animationDuration: '350ms, 150ms',
+      animationTimingFunction:
+        'token(easings.expressive-fast-spatial), token(easings.expressive-fast-effects)'
+    }
+  },
+  'slide-from-bottom-fade-in': {
+    value: {
+      animationName: 'slide-from-bottom, fade-in',
+      animationDuration: '500ms, 200ms',
+      animationTimingFunction:
+        'token(easings.expressive-default-spatial), token(easings.expressive-default-effects)'
+    }
+  },
+  'slide-to-bottom-fade-out': {
+    value: {
+      animationName: 'slide-to-bottom, fade-out',
+      animationDuration: '350ms, 150ms',
+      animationTimingFunction:
+        'token(easings.expressive-fast-spatial), token(easings.expressive-fast-effects)'
+    }
+  },
+  'slide-from-right-full-fade-in': {
+    value: {
+      animationName: 'slide-from-right-full, fade-in',
+      animationDuration: '500ms, 200ms',
+      animationTimingFunction:
+        'token(easings.expressive-default-spatial), token(easings.expressive-default-effects)'
+    }
+  },
+  'slide-to-right-full-fade-out': {
+    value: {
+      animationName: 'slide-to-right-full, fade-out',
+      animationDuration: '350ms, 150ms',
+      animationTimingFunction:
+        'token(easings.expressive-fast-spatial), token(easings.expressive-fast-effects)'
+    }
+  },
+  'slide-from-left-full-fade-in': {
+    value: {
+      animationName: 'slide-from-left-full, fade-in',
+      animationDuration: '500ms, 200ms',
+      animationTimingFunction:
+        'token(easings.expressive-default-spatial), token(easings.expressive-default-effects)'
+    }
+  },
+  'slide-to-left-full-fade-out': {
+    value: {
+      animationName: 'slide-to-left-full, fade-out',
+      animationDuration: '350ms, 150ms',
+      animationTimingFunction:
+        'token(easings.expressive-fast-spatial), token(easings.expressive-fast-effects)'
+    }
+  },
+  'slide-from-top-full-fade-in': {
+    value: {
+      animationName: 'slide-from-top-full, fade-in',
+      animationDuration: '500ms, 200ms',
+      animationTimingFunction:
+        'token(easings.expressive-default-spatial), token(easings.expressive-default-effects)'
+    }
+  },
+  'slide-to-top-full-fade-out': {
+    value: {
+      animationName: 'slide-to-top-full, fade-out',
+      animationDuration: '350ms, 150ms',
+      animationTimingFunction:
+        'token(easings.expressive-fast-spatial), token(easings.expressive-fast-effects)'
+    }
+  },
+  'slide-from-bottom-full-fade-in': {
+    value: {
+      animationName: 'slide-from-bottom-full, fade-in',
+      animationDuration: '500ms, 200ms',
+      animationTimingFunction:
+        'token(easings.expressive-default-spatial), token(easings.expressive-default-effects)'
+    }
+  },
+  'slide-to-bottom-full-fade-out': {
+    value: {
+      animationName: 'slide-to-bottom-full, fade-out',
+      animationDuration: '350ms, 150ms',
+      animationTimingFunction:
+        'token(easings.expressive-fast-spatial), token(easings.expressive-fast-effects)'
+    }
+  }
+})
