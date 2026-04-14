20a5ec8553a75077d80ee3608fc3eef6c08f0261
die-goncalves <die.goncalves1990@gmail.com>
2026-04-14 11:54:50 -0300

    refactor(utils): improve parsePoint

--- src/utils/parse-point.ts
+++ src/utils/parse-point.ts
@@ -1,8 +1,12 @@
-export const parsePoint = (pointStr: string) => {
-  if (!pointStr) return { lat: null, lng: null }
-  const [lat, lng] = pointStr.replace(/[()]/g, '').split(',')
-  return {
-    lat: Number(lat),
-    lng: Number(lng)
-  }
+export const parsePoint = (pointStr: string | null) => {
+  if (!pointStr) return null
+
+  const [latStr, lngStr] = pointStr.replace(/[()]/g, '').split(',')
+
+  const lat = Number(latStr)
+  const lng = Number(lngStr)
+
+  if (Number.isNaN(lat) || Number.isNaN(lng)) return null
+
+  return { lat, lng }
 }
