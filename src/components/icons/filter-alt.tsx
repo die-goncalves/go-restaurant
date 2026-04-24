ddf53c8383923b632d75b1d550f310eb2de23a3e
die-goncalves <die.goncalves1990@gmail.com>
2026-04-24 13:54:49 -0300

    feat(icons): add filter alt component

---
+++ src/components/icons/filter-alt.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from 'react'
+
+export function FilterAltIcon(props: ComponentProps<'svg'>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      preserveAspectRatio="xMidYMid meet"
+      aria-hidden="true"
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M400-160v-280L118-800h724L560-440v280H400Zm80-308 198-252H282l198 252Zm0 0Z" />
+    </svg>
+  )
+}
